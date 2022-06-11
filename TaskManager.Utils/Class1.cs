c9e0283052223bcc2909c14e6c64bc1be668e00f
P.Konstantin <kostya.potekaev@gmail.com>
2022-06-11 17:12:08 +0300

    Создан новый класс получения короткого пути

---
+++ TaskManager.Utils/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace TaskManager.Utils
+{
+    public static class API
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern uint GetShortPathName(
+        [MarshalAs(UnmanagedType.LPTStr)]
+        string lpszLongPath,
+        [MarshalAs(UnmanagedType.LPTStr)]
+        StringBuilder lpszShortPath,
+        uint cchBuffer);
+    }
+}
